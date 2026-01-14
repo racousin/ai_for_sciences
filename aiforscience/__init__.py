f0c551226c75facc954f1415dc291bef31c4a0a5
Raphael Cousin <racousin@student.42.fr>
2026-01-14 13:57:29 +0100

    up

--- aiforscience/__init__.py
+++ aiforscience/__init__.py
@@ -6,11 +6,15 @@ from .visualization import (
     plot_predictions,
     plot_gradient_step,
     plot_nonlinear_data,
+    plot_attention_weights,
+    plot_embeddings_2d,
 )
 
 from .display import (
     print_model_params,
     print_device_comparison,
+    print_tokenization,
+    print_model_summary,
 )
 
 from .data import (
@@ -25,9 +29,13 @@ __all__ = [
     "plot_predictions",
     "plot_gradient_step",
     "plot_nonlinear_data",
+    "plot_attention_weights",
+    "plot_embeddings_2d",
     # Display
     "print_model_params",
     "print_device_comparison",
+    "print_tokenization",
+    "print_model_summary",
     # Data
     "generate_linear_data",
     "generate_nonlinear_data",
